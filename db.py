c9b257e13f3c9619ae0fe36632dd5d7a8a16f97f
leslyal <leslyalc@bu.edu>
2023-03-30 10:42:08 -0400

    Database created Successfully :)

---
+++ db.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import sqlite3
+
+
+# create a connection to the SQLite database
+conn = sqlite3.connect('p2p_chat.db')
+
+# create the client table
+df_client = pd.DataFrame(columns=['IP', 'port', 'status'])
+df_client.to_sql('client', conn, if_exists='replace', index=False)
+
+# create the messages table
+df_messages = pd.DataFrame(columns=['from', 'to', 'msg', 'time', 'isSent'])
+df_messages.to_sql('messages', conn, if_exists='replace', index=False)
+
+# close the connection to the database
+conn.close()
+
+
+
+
+
