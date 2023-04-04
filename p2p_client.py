2d629841611dce6b50ce1c020a3bfd3695f105f4
leslyal <leslyalc@bu.edu>
2023-04-04 00:23:15 -0400

    Added error handling

--- p2p_client.py
+++ p2p_client.py
@@ -10,23 +10,46 @@ peer_port = int(input("Enter port of other client: "))
 
 # Connect to other client
 peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-peer_socket.connect((peer_ip, peer_port))
+try:
+    peer_socket.connect((peer_ip, peer_port))
+except socket.error as e:
+    print(f"Error connecting to {peer_ip}:{peer_port}")
+    sys.exit()
 
 # Get IP and port of current client
+if len(sys.argv) < 3:
+    print("Usage: python p2p_client.py <IP address> <port>")
+    sys.exit()
+
 my_ip = sys.argv[1]
 my_port = int(sys.argv[2])
 
 c = conn.cursor()
 
 while True:
-    message = input('Enter message: ')
+    try:
+        message = input('Enter message: ')
 
-    # Insert message into messages table
-    c.execute("INSERT INTO messages (sender, receiver, msg, time, isSent) VALUES (?, ?, ?, datetime('now'), ?)", (my_ip, peer_port, message, 1))
-    conn.commit()
+        # Insert message into messages table
+        c.execute("INSERT INTO messages (sender, receiver, msg, time, isSent) VALUES (?, ?, ?, datetime('now'), ?)", (my_ip, peer_port, message, 1))
+        conn.commit()
 
-    # send message to other client
-    peer_socket.send(message.encode())
+        # send message to other client
+        peer_socket.send(message.encode())
 
-    response = peer_socket.recv(1024).decode()
-    print('Received:', response)
+        response = peer_socket.recv(1024).decode()
+        print('Received:', response)
+
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupt. Closing connection.")
+        peer_socket.close()
+        sys.exit()
+
+    except socket.error as e:
+        print(f"\nError sending/receiving data: {e}")
+        peer_socket.close()
+        sys.exit()
+
+    except sqlite3.Error as e:
+        print(f"\nError inserting data into database: {e}")
+        sys.exit()
