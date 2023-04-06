0f7dc7831849b6b504f40adc4dcf19a9c5fa9375
jlatina <jivillan@bu.edu>
2023-04-06 09:03:44 -0400

    update code

--- p2p_client.py
+++ p2p_client.py
@@ -4,9 +4,20 @@ import sqlite3
 
 conn = sqlite3.connect('p2p_chat.db')
 
+# Get IP and port of current client
+if len(sys.argv) < 3:
+    print("Usage: python p2p_client.py <IP address> <port>")
+    sys.exit()
+
+my_ip = sys.argv[1]
+my_port = int(sys.argv[2])
+
+c = conn.cursor()
+
 # Prompt user for IP and port of second client
 peer_ip = input("Enter IP address of other client: ")
 peer_port = int(input("Enter port of other client: "))
+
 
 # Connect to other client
 peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,10 +27,10 @@ except socket.error as e:
     print(f"Error connecting to {peer_ip}:{peer_port}")
     sys.exit()
 
-my_ip = socket.gethostbyname(socket.gethostname())
-my_port = 5000
+# my_ip = socket.gethostbyname(socket.gethostname())
+# my_port = 5000
 
-c = conn.cursor()
+#c = conn.cursor()
 
 while True:
     try:
@@ -32,11 +43,22 @@ while True:
         # send message to other client
         peer_socket.send(message.encode())
 
+         # Listen for incoming connections from other clients
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.bind((my_ip, my_port))
+        server_socket.listen()
+
+        client_socket, client_address = server_socket.accept()
+
         # Waits for a response from the other client, 
         # which is assumed to be no more than 1024 bytes long, 
         # and decodes it from bytes to a string.
+        # Receive message from other client
         response = peer_socket.recv(1024).decode()
         print('Received:', response)
+
+        client_socket.close()
+        server_socket.close()
 
     except KeyboardInterrupt:
         print("\nKeyboard interrupt. Closing connection.")
