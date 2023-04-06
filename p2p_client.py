86db7eada913c36eca3af3a6caf1d0749f70eb02
leslyal <leslyalc@bu.edu>
2023-04-06 09:33:19 -0400

    Keep server running

--- p2p_client.py
+++ p2p_client.py
@@ -1,10 +1,11 @@
 import socket
 import sys
 import sqlite3
+import threading
 
 conn = sqlite3.connect('p2p_chat.db')
 
-my_ip = socket.gethostbyname(socket.gethostname())
+my_ip = '172.20.10.5'
 my_port = 5000
 
 c = conn.cursor()
@@ -18,8 +19,54 @@ peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     peer_socket.connect((peer_ip, peer_port))
 except socket.error as e:
-    print(f"Error connecting to {peer_ip}:{peer_port}")
+    print(f"Error connecting to {peer_ip}:{peer_port}: {e}")
     sys.exit()
+
+# Setup server socket to listen for incoming connections
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((my_ip, my_port))
+server_socket.listen()
+
+def handle_client(client_socket):
+    while True:
+        try:
+            # Receive message from client
+            message = client_socket.recv(1024).decode()
+            print('Received:', message)
+
+            # Insert message into messages table
+            c.execute("INSERT INTO messages (sender, receiver, msg, time, isSent) VALUES (?, ?, ?, datetime('now'), ?)", (peer_ip, my_port, message, 1))
+            conn.commit()
+
+            # Send response to client
+            response = input('Enter response: ')
+            client_socket.send(response.encode())
+
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt. Closing connection.")
+            client_socket.close()
+            break
+
+        except socket.error as e:
+            print(f"\nError sending/receiving data: {e}")
+            client_socket.close()
+            break
+
+        except sqlite3.Error as e:
+            print(f"\nError inserting data into database: {e}")
+            client_socket.close()
+            break
+
+# Listen for incoming connections from other clients in a separate thread
+def listen_for_clients():
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Accepted connection from {client_address}")
+        client_thread = threading.Thread(target=handle_client, args=(client_socket,))
+        client_thread.start()
+
+client_listener_thread = threading.Thread(target=listen_for_clients)
+client_listener_thread.start()
 
 while True:
     try:
@@ -32,33 +79,20 @@ while True:
         # send message to other client
         peer_socket.send(message.encode())
 
-         # Listen for incoming connections from other clients
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_socket.bind((my_ip, my_port))
-        server_socket.listen()
-
-        client_socket, client_address = server_socket.accept()
-
-        # Waits for a response from the other client, 
-        # which is assumed to be no more than 1024 bytes long, 
-        # and decodes it from bytes to a string.
-        # Receive message from other client
-        response = peer_socket.recv(1024).decode()
-        print('Received:', response)
-
-        client_socket.close()
-        server_socket.close()
-
     except KeyboardInterrupt:
         print("\nKeyboard interrupt. Closing connection.")
         peer_socket.close()
+        server_socket.close()
         sys.exit()
 
     except socket.error as e:
         print(f"\nError sending/receiving data: {e}")
         peer_socket.close()
+        server_socket.close()
         sys.exit()
 
     except sqlite3.Error as e:
         print(f"\nError inserting data into database: {e}")
+        peer_socket.close()
+        server_socket.close()
         sys.exit()
